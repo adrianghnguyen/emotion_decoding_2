ceadacd0b83d0606a6d03bcfb2abecfc1da3508b
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-25 18:07:47 -0500

    Changed input_text to local_storage key to use the inner text element which is less likely to break than passing in the template.

---
+++ templates/js/main_local_storage.js
@@ -0,0 +1,10 @@
+function process_local_storage{
+    const local_keys = Object.keys(localStorage);
+    
+    // Do stuff for each saved session
+    local_keys.forEach(individual_session => {
+        //function1
+        //function2
+    });
+}
+
