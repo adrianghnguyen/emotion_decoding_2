0393522fc4f84eb69901fbdef19c7e9ea4c91599
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-25 18:55:54 -0500

    First commit - local main storage

--- static/main_local_storage.js
+++ static/main_local_storage.js
@@ -1,10 +1,18 @@
-function process_local_storage(){
-    const local_keys = Object.keys(localStorage);
+function process_local_storage() {
+    const all_local_keys = Object.keys(localStorage);
     
-    // Do stuff for each saved session
-    local_keys.forEach(individual_session => {
-        //function1
-        //function2
+    // Do stuff for each saved submission entry
+    all_local_keys.forEach(individual_key => {
+        // Declare variables using var, let, or const
+        const raw_data_payload = localStorage.getItem(individual_key);
+        const parsed_data = JSON.parse(raw_data_payload);
+        process_data(parsed_data);
     });
 }
 
+function process_data(parsed_data) {
+    console.log(parsed_data);
+}
+
+console.log('Processing local storage script')
+process_local_storage();
