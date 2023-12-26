553e3c4f47caec18320a0ff2cc5e87083f33f54d
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-25 20:04:29 -0500

    initial working version of printout using stringify in all results

--- static/main_local_storage.js
+++ static/main_local_storage.js
@@ -18,20 +18,24 @@ function process_local_storage() {
 function process_data_html(parsed_data) {
     timestamp = parsed_data.timestamp
     original_input = parsed_data.inputText
-    arr_emotion_results = parsed_data.emotion_results
+    payload_emotion_results = parsed_data.emotionResults
 
     var html_content = `<div>`
     html_content += `<h3>${timestamp}</h3>`;
     html_content += `Input text: ${original_input}<br>`;
 
     // Emotions need to be processed by each individual entry
-    emotion_print = JSON.stringify(arr_emotion_results, null, 4)
-    html_content += `<br>${emotion_print}<br>`
-    html_content += `</div>`
+    payload_emotion_results.forEach(emotion_result =>{
+        html_content += JSON.stringify(emotion_result) //Stringified otherwise renders as object object
+    })
 
-    console.log(html_content)
+    // emotion_print = JSON.stringify(arr_emotion_results, null, 4)
+    // html_content += `<br>${emotion_print}<br>`
+    // html_content += `</div>`
     return html_content
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     process_local_storage();
