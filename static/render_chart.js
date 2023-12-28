6cac002927cbb4cdbb240e3396c72f5cd53a645c
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-27 19:04:07 -0500

    Initial radar chart with placeholder

--- static/render_chart.js
+++ static/render_chart.js
@@ -1,22 +1,50 @@
-function renderChart() {
-  const ctx = document.getElementById('myChart');
+// Unused for the moment - will look into moving the spider chart rendering later
+const data = {
+  labels: [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+    'Cycling',
+    'Running'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 90, 81, 56, 55, 40],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'My Second Dataset',
+    data: [28, 48, 40, 19, 96, 27, 100],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }]
+};
 
-    new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
+function renderChart(data) {
+  const ctx = document.getElementById('myChart');
+  const config = {
+    type: 'radar',
+    data: data,
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
+      elements: {
+        line: {
+          borderWidth: 3
         }
       }
     }
-    });
+  };
+
+  new Chart(ctx, config);
 }
