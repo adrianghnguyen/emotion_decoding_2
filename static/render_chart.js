57c9ea579140aab88ded519a0dd235a652e4b90e
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-28 03:54:14 -0500

    Modified filtering logic to use the original encoding model's object value of 'filter: true'

--- static/render_chart.js
+++ static/render_chart.js
@@ -48,22 +48,14 @@ function createRadarChart(labels_var, data_var, chart_title, element_id)
 }
 
 function render_latest_radar(chart_title, element_id) {
-    //Retrieves the latest key in localStorage and calls the generic createRadarFunction to generate a chart
-    const latest_key = Object.keys(localStorage).sort()[0]; // Grab one key
-    const raw_data_payload = localStorage.getItem(latest_key);
-    const parsed_data_emotion_results = JSON.parse(raw_data_payload).emotionResults;
+    const parsed_data_emotion_results = get_latest_emotion_result() // Retrieved from local_storage
 
     var data_array = [];
     var labels_array = [];
 
-    // Get all the pairs of values for the (emotion,score) pairs
+    // Placing the data into the correct format for the radar chart to be created
     parsed_data_emotion_results.forEach((individual_result) => {
-//        console.log(individual_result.score);
-//        console.log(individual_result.emotion);
-
-        // Assuming you want to push these values into arrays
-        // TODO: Remove or modify this filtering scheme
-        if(individual_result.score > 0.1){
+        if(individual_result.filter){
             data_array.push(individual_result.score);
         }
         else {
@@ -79,8 +71,6 @@ function render_latest_radar(chart_title, element_id) {
     //Make the chart
     createRadarChart(labels_array, data_array, chart_title, element_id)
 }
-
-
 
 function createRadarSliceData(emotion_result, category) {
   const data = [];
@@ -99,51 +89,4 @@ function createRadarSliceData(emotion_result, category) {
   };
 
   return radar_slice;
-}
-
-function createMultiRadarChart(element_id) // labels_var, dataset_var, element_id  
-{
-  const emotion_result = get_latest_emotion_result();
-  const test_labels_var = emotion_result.map(result => result.emotion);
-  const emotion_categories = Array.from(new Set(emotion_result.map(result => result.emotion_category)));
-  
-  // Iterate over each emotion category and create radar slices
-  // for (const category of emotion_categories) {
-  //     var resultObject = createRadarSlice(emotion_result, category)
-  //     dataset_var.push(resultObject);    
-  //   };
-  
-  var resulting_slice = createRadarSliceData(emotion_result, 'ambiguous')
-  console.log(resulting_slice)
-  dataset_var = [resulting_slice]
-
-    const data = {
-      labels: test_labels_var,
-      datasets: dataset_var,
-    };
-
-  const config = {
-    type: 'radar',
-    data: data,
-    options: {
-      spanGaps: false,
-      scales: {
-          r: {
-              angleLines: {
-                  display: false
-              },
-              suggestedMin: 0,
-              suggestedMax: 1
-          }
-      },
-      elements: {
-        line: {
-          borderWidth: 2
-        }
-      }
-    },
-  };
-
-   const ctx = document.getElementById(element_id).getContext('2d');
-   new Chart(ctx, config);
 }
