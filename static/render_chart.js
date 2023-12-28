927c4336878525ae222b0bf1fdc7bdbe11a93eed
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-27 19:24:38 -0500

    Moved radar chart into its own script

--- static/render_chart.js
+++ static/render_chart.js
@@ -1,40 +1,32 @@
-// Unused for the moment - will look into moving the spider chart rendering later
-const data = {
-  labels: [
-    'Eating',
-    'Drinking',
-    'Sleeping',
-    'Designing',
-    'Coding',
-    'Cycling',
-    'Running'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 90, 81, 56, 55, 40],
-    fill: true,
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgb(255, 99, 132)',
-    pointBackgroundColor: 'rgb(255, 99, 132)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(255, 99, 132)'
-  }]
-};
+function createRadarChart(labels_var, data_var, chart_title, element_id)
+{
+    const data = {
+      labels: labels_var,
+      datasets: [{
+        label: chart_title,
+        data: data_var,
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)'
+      }]
+    };
 
-function renderChart(data) {
-  const ctx = document.getElementById('myChart');
-  const config = {
-    type: 'radar',
-    data: data,
-    options: {
-      elements: {
-        line: {
-          borderWidth: 3
+    const config = {
+      type: 'radar',
+      data: data,
+      options: {
+        elements: {
+          line: {
+            borderWidth: 2
+          }
         }
-      }
-    }
-  };
+      },
+    };
 
-  new Chart(ctx, config);
+    const ctx = document.getElementById(element_id).getContext('2d');
+    new Chart(ctx, config);
 }
