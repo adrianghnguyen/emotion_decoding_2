b44a02cf617fb8ac3c7000c4faee82a28ce35770
Adrian Nguyen <tilounguyen@hotmail.com>
2024-01-06 17:55:53 -0500

    Fixed timeline chart to work with the new method of calculating average category

--- static/render_chart.js
+++ static/render_chart.js
@@ -2,14 +2,14 @@
 // This function should be replaced by local_storage.js functions
 function retrieveLatestStoredLocalStorage() {
 
-    const all_keys = Object.keys(localStorage).sort(); // Sorting in ascending key value
-    const latest_key = all_keys[all_keys.length - 1] //
+  const all_keys = Object.keys(localStorage).sort(); // Sorting in ascending key value
+  const latest_key = all_keys[all_keys.length - 1] //
 
-    const latest_data_payload = localStorage.getItem(latest_key);
-    const parsed_data = JSON.parse(latest_data_payload)
+  const latest_data_payload = localStorage.getItem(latest_key);
+  const parsed_data = JSON.parse(latest_data_payload)
 
-    console.log('Retrieving latest stored payload from local storage')
-    return parsed_data
+  console.log('Retrieving latest stored payload from local storage')
+  return parsed_data
 }
 
 // Processes the local storage object to only return the emotionResults object
@@ -34,10 +34,10 @@ function createRadarChart(labels_var, data_var, chart_title, element_id) {
     type: 'radar',
     data: data,
     options: {
-    spanGaps: false,
+      spanGaps: false,
       scales: {
         r: {
-          grid:{
+          grid: {
             circular: true,
           },
           ticks: {
@@ -111,21 +111,21 @@ function sorted_emotions(emotions_results) {
 // ---------------------------------------------------- RADAR FUNCTIONS ----------------------------------------------------
 // Creates radar chart which clusters for the 4 higher level sentiment positive/negative/neutral/amb
 function radarEmotionCategories(emotion_result_object, element_id) { // Right now hard-coded to do it off the latest results
-    console.log('Creating higher-level emotion categories categories')
-    const higher_emotion_categories = ['positive','ambiguous','negative','neutral'] // Using hard-coded values to force radar to output in a specific and create axes. Positive needs to be opposite of negative.
+  console.log('Creating higher-level emotion categories categories')
+  const higher_emotion_categories = ['positive', 'ambiguous', 'negative', 'neutral'] // Using hard-coded values to force radar to output in a specific and create axes. Positive needs to be opposite of negative.
 
-    //Prep the data for the chart
-    filtered_emotion_object = emotion_result_object.filter((emotion_result) => emotion_result.filter)     // Remove zero-values of emotion_scores dragging down the average
+  //Prep the data for the chart
+  filtered_emotion_object = emotion_result_object.filter((emotion_result) => emotion_result.filter)     // Remove zero-values of emotion_scores dragging down the average
 
-    // Need to align it with the labels position
-    chart_data = allAverageCategoryScores(filtered_emotion_object, higher_emotion_categories) // Creates data format appropriate for chart.js since it looks at the position with the labels
-    sorted_chart_data = []
-    for(category of higher_emotion_categories){
-      sorted_chart_data.push(chart_data[category])
-    }
-  
-    console.log(sorted_chart_data)
-    createRadarChart(higher_emotion_categories, sorted_chart_data, 'Category average', element_id)
+  // Need to align it with the labels position
+  chart_data = allAverageCategoryScores(filtered_emotion_object, higher_emotion_categories) // Creates data format appropriate for chart.js since it looks at the position with the labels
+  sorted_chart_data = []
+  for (category of higher_emotion_categories) {
+    sorted_chart_data.push(chart_data[category])
+  }
+
+  console.log(sorted_chart_data)
+  createRadarChart(higher_emotion_categories, sorted_chart_data, 'Category average', element_id)
 }
 
 // Detailed radar chart for all 28 emotion scores - TBH this chart isn't very useful
@@ -159,62 +159,56 @@ function radarDetailed(obj_emotion_result, element_id) {
 
 // ---------------------------------------------------- LINE CHART FUNCTIONS ----------------------------------------------------
 // Takes in all data retrieved from local storage and display as a line chart
-function lineChartTimelineCategory(all_data, element_name){
-	console.log('Creating timeline linechart')
+function lineChartTimelineCategory(all_data, element_name) {
+  console.log('Creating timeline linechart')
 
-	var all_average_scores = []
-	var x_axis = []
+  var all_average_scores = []
+  const higher_emotion_categories = ['positive', 'negative', 'ambiguous', 'neutral']
+  var x_axis = []
 
-    // Go through each user-submitted entry log
-	for (let entry of all_data){
+  // Go through each user-submitted entry log
+  category_timeline_values = {}
+  for (let entry of all_data) {
 
-	    // Break down the individual emotion scores data into respective category averages
-        category_scores = convertResultSentimentAverage(entry.emotionResults)
-        all_average_scores.push(category_scores)
+    // Get all the average values as an array in chronological order
+    category_averages_dict = allAverageCategoryScores(entry.emotionResults, higher_emotion_categories)
+    for (category of higher_emotion_categories) {
 
-	    //Timestamp
-	    timestamp = entry.timestamp
-	    x_axis.push(timestamp)
-	}
+      // Initialize the array if not already done
+      if (!category_timeline_values[category]) {
+        category_timeline_values[category] = [];
+      }
 
-	// Break down the average scores into their own arrays
-    // Initialize an object to store arrays for each category
-    const categorizedArrays = {};
-
-    all_average_scores.forEach(data => {
-        data.average_category_score.forEach(categoryScore => {
-            const category = categoryScore.category;
-
-            // If the category array doesn't exist, create it
-            if (!categorizedArrays[category]) {
-                categorizedArrays[category] = [];
-            }
-
-            // Push the category score to the corresponding array
-            categorizedArrays[category].push(categoryScore.average_score);
-        });
-    });
-
-    // Now categorizedArrays contains separate arrays for each category
-    var dataset_array = []
-    for (let [category, data] of Object.entries(categorizedArrays)) {
-        let dataset_entry = {
-            label: category,
-            data: data,
-            tension: 0.1,
-//            spanGaps: false
-        };
-        dataset_array.push(dataset_entry);
+      console.log(category_averages_dict[category])
+      category_timeline_values[category].push(category_averages_dict[category])
     }
 
-	datasets = dataset_array
-	console.log(datasets)
+    //Timestamp
+    timestamp = entry.timestamp
+    x_axis.push(timestamp)
+  }
+
+  console.log(category_timeline_values)
+
+  temp_dataset_array = []
+
+  for (const [category,data_values] of Object.entries(category_timeline_values)){
+    let individual_dataset = {
+      label: category,
+      data: data_values,
+      tension: 0.1
+    }
+
+    temp_dataset_array.push(individual_dataset)
+  }
+
+  datasets = temp_dataset_array
 
     // Create the chart
-	data = {
-		labels: x_axis,
-		datasets: datasets
-	}
+  data = {
+  	labels: x_axis,
+  	datasets: datasets
+  }
     const config = {
         type: 'line',
         data: data,
@@ -256,22 +250,19 @@ function calculateAverage(array) {
 // Takes list of categories and returns an array of their averages in the passed order
 function allAverageCategoryScores(emotion_object, list_of_categories) {
 
-  temp_all_average_scores = []
-
-  array_nb = list_of_categories.length
   all_average_scores = {}
 
-  for(category of list_of_categories){
+  for (category of list_of_categories) {
     all_average_scores[category] = []
   }
 
   // Get all the respective results for a specific category and accumulates it into an array
-  for (line of emotion_object){
-      for(category of list_of_categories){
-        if(category == line.emotion_category){
-          all_average_scores[category].push(line.score)
-        }
+  for (line of emotion_object) {
+    for (category of list_of_categories) {
+      if (category == line.emotion_category) {
+        all_average_scores[category].push(line.score)
       }
+    }
   }
 
   // Store it as an object to make it independent of order that the values were placed in
@@ -286,34 +277,34 @@ function allAverageCategoryScores(emotion_object, list_of_categories) {
 // Takes list of categories and returns an array of their averages in the passed order
 function allAverageCategoryScores_v2(emotion_object, list_of_categories) {
 
-	temp_all_average_scores = []
-  
-	for (category of list_of_categories) {
-		result = averageCategoryScore(emotion_object, category)
-		entry = {
-			category: category,
-			average_score: result
-		}
+  temp_all_average_scores = []
 
-	  temp_all_average_scores.push(entry)
-	}
-  
-	return temp_all_average_scores
-  }
-
-// Take emotion_result object and computes the category averages which is returned for the timeline
-function convertResultSentimentAverage(emotion_result_object){
-    // Hard-coded to specify a return order
-    const higher_emotion_categories = ['positive', 'negative', 'ambiguous', 'neutral'] // This will become the label
-
-    //Prep the data for the chart
-    filtered_emotion_object = emotion_result_object.filter((emotion_result) => emotion_result.filter)   // Remove zero-values of emotion_scores dragging down the average
-    averaged_sentiment_results = allAverageCategoryScores_v2(filtered_emotion_object, higher_emotion_categories)
-
-    computed_averages = {
-        average_category_score: averaged_sentiment_results
+  for (category of list_of_categories) {
+    result = averageCategoryScore(emotion_object, category)
+    entry = {
+      category: category,
+      average_score: result
     }
 
-    return computed_averages
+    temp_all_average_scores.push(entry)
+  }
+
+  return temp_all_average_scores
+}
+
+// Take emotion_result object and computes the category averages which is returned for the timeline
+function convertResultSentimentAverage(emotion_result_object) {
+  // Hard-coded to specify a return order
+  const higher_emotion_categories = ['positive', 'negative', 'ambiguous', 'neutral'] // This will become the label
+
+  //Prep the data for the chart
+  filtered_emotion_object = emotion_result_object.filter((emotion_result) => emotion_result.filter)   // Remove zero-values of emotion_scores dragging down the average
+  averaged_sentiment_results = allAverageCategoryScores_v2(filtered_emotion_object, higher_emotion_categories)
+
+  computed_averages = {
+    average_category_score: averaged_sentiment_results
+  }
+
+  return computed_averages
 }
 
